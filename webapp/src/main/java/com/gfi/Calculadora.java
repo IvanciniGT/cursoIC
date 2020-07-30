53654c393f9cce6baf4e9c670436aea06c00d2d8
IvanciniGT <ivan.osuna.ayuste@gmail.com>
2020-07-30 16:01:33 +0000

    sonar

--- webapp/src/main/java/com/gfi/Calculadora.java
+++ webapp/src/main/java/com/gfi/Calculadora.java
@@ -1,7 +1,11 @@
 package com.gfi;
 
 public class Calculadora{
-
+    
+    /**
+     * Esta funcioón suma 2 numeros
+     * @return El resultado de la suma
+     */
     public static int suma(int numero1, int numero2){
         return numero1+numero2; // No escribo el codigo
     }
--- webapp/src/test/java/com/gfi/CalculadoraTest.java
+++ webapp/src/test/java/com/gfi/CalculadoraTest.java
@@ -1,51 +1,37 @@
 package com.gfi;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Unit test for simple App.
  */
 public class CalculadoraTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public CalculadoraTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( CalculadoraTest.class );
-    }
-
+    @Test
     public void testSumarPositivos()
     {
         int resultadoEsperado=14;
         int resultado=Calculadora.suma(9,5);
         assertEquals(resultado,resultadoEsperado );
     }
+    @Test
     public void testSumarNegativos()
     {
         int resultadoEsperado=14;
         int resultado=Calculadora.suma(19,-5);
         assertEquals(resultado,resultadoEsperado );
     }
+    @Test
     public void testSumarCero()
     {
         int resultadoEsperado=14;
         int resultado=Calculadora.suma(14,0);
         assertEquals(resultado,resultadoEsperado );
     }
+    @Test
     public void testSumarDosNegativos()
     {
         int resultadoEsperado=-14;
